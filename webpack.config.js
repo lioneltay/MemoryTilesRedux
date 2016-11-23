b2ae0267aba4661c4960c669dab497167ded4d2c
Lionel Tay <lionel.tay@hotmail.com>
2016-11-23 12:24:50 +1100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+			test: /.jsx?$/,
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+		inline: true,
+    contentBase: './'
+  }
+};
